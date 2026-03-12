1c6d980a95f51797d1d4dc088649f5c86b0f96b7
Tom Kirby <tom@k87.co.uk>
2026-03-12 17:02:57 +0000

    Customized the packages and built out the init script

--- init.sh
+++ init.sh
@@ -2,15 +2,17 @@
 
 
 # update the system
-
+sudo pacman -Syu
 
 # Install yay
 
 
 # Install base packages
+echo "=====INSTALLING BASE PACKAGES"
 yay -S --needed - < packages/base_packages.txt
 
 # symlink the configuration
+echo "=====ADDING STOW SYMLINK FOR DOT FILES"
 stow waybar
 stow hyprland
 stow kitty
