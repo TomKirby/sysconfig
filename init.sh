45c741f1e0f8251359bed18828d627e6d24db9d3
Tom Kirby <tom@k87.co.uk>
2026-03-13 18:15:42 +0000

    updated build script

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # update the system
 sudo pacman -Syu
 
@@ -9,16 +8,9 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 
-
 # Install base packages
 echo "=====INSTALLING BASE PACKAGES"
 yay -S --needed - < packages/base_packages.txt
-
-# symlink the configuration
-echo "=====ADDING STOW SYMLINK FOR DOT FILES"
-stow waybar
-stow hyprland
-stow kitty
 
 # Install Standard User Applications
 
